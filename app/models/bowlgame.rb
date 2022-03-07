aabc7966123647a13d40e5f7fec08adb8538c6be
lizajohnsonbooth <liza.johnson@chicagobooth.edu>
2022-03-07 23:31:17 +0000

    Family grid almost done

--- app/models/bowlgame.rb
+++ app/models/bowlgame.rb
@@ -17,7 +17,7 @@
 #
 class Bowlgame < ApplicationRecord
   has_many(:picks, { :class_name => "Pick", :foreign_key => "bowl_game_id", :dependent => :destroy })
-  belongs_to(:home_team, { :required => true, :class_name => "Team", :foreign_key => "team_one_id" })
-  belongs_to(:away_team, { :required => true, :class_name => "Team", :foreign_key => "team_two_id" })
+  belongs_to(:team_one, { :required => true, :class_name => "Team", :foreign_key => "team_one_id" })
+  belongs_to(:team_two, { :required => true, :class_name => "Team", :foreign_key => "team_two_id" })
   belongs_to(:winner, { :required => false, :class_name => "Team", :foreign_key => "winner_id" })
 end
